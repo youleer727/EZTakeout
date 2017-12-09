af7076008fbaf4a64239637463b9c0e4cb982eef
vskyno6 <yq2212@columbia.edu>
2017-12-08 21:28:08 -0500

    modified for single month reduce

--- model/month_day_reducer.py
+++ model/month_day_reducer.py
@@ -13,9 +13,6 @@ conf = SparkConf().setAppName("EECS6893")
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
-YEAR = 2016
-MONTH = 6
-
 
 def parse_month(i):
     if (i < 10):
@@ -60,27 +57,34 @@ def reducer():
     year = 2016
     month = 6
     while (True):
-        input_file = str(year) + '-' + parse_month(month) + '.extracted.csv'
-        input_path = datasets_path + trip_data_suffix + input_file
-        trip_file = sc.textFile(input_path)
-        trip_file_header = trip_file.filter(lambda l: 'tpep_pickup_datetime' in l)
-        trip_file = trip_file.subtract(trip_file_header)
-        lines = trip_file.map(lambda line: line.split(','))
-        RDD = lines.map(lambda cols: (
-        # Note that per now we do not include dropoff time
-        generate_new_file_name(cols[0]), (parse_start_time(cols[0]), int(cols[2]), float(cols[3]), float(cols[4]), calculate_time(cols[0], cols[1])))).reduceByKey(lambda a, b: appendAB(a, b))
-        sqlDF = sqlContext.createDataFrame(RDD, ['fileName', 'data'])
-        dfMatrix = sqlDF.toPandas().as_matrix()
-        for i in range(0, dfMatrix.shape[0]):
-            row = dfMatrix[i]
-            df = DataFrame(row[1], columns=['start_hour', 'passenger_count', 'trip_distance', 'total_amount', 'time_spent'])
-            df.to_csv(path_or_buf=(datasets_path + 'processed/' + 'tripdata_reduced/' + row[0] + '.csv').encode('ascii', 'ignore'), index=False)
+        single_reduce(year, month)
         month += 1
         if(month == 13):
             year = 2017
             month = 1
         elif(month == 7):
             break
+
+
+def single_reduce(year, month):
+    input_file = str(year) + '-' + parse_month(month) + '.extracted.csv'
+    input_path = datasets_path + trip_data_suffix + input_file
+    trip_file = sc.textFile(input_path)
+    trip_file_header = trip_file.filter(lambda l: 'tpep_pickup_datetime' in l)
+    trip_file = trip_file.subtract(trip_file_header)
+    lines = trip_file.map(lambda line: line.split(','))
+    RDD = lines.map(lambda cols: (
+        # Note that per now we do not include dropoff time
+        generate_new_file_name(cols[0]), (parse_start_time(cols[0]), int(cols[2]), float(cols[3]), float(cols[4]),
+                                          calculate_time(cols[0], cols[1])))).reduceByKey(lambda a, b: appendAB(a, b))
+    sqlDF = sqlContext.createDataFrame(RDD, ['fileName', 'data'])
+    dfMatrix = sqlDF.toPandas().as_matrix()
+    for i in range(0, dfMatrix.shape[0]):
+        row = dfMatrix[i]
+        df = DataFrame(row[1], columns=['start_hour', 'passenger_count', 'trip_distance', 'total_amount', 'time_spent'])
+        df.to_csv(path_or_buf=(datasets_path + 'processed/' + 'tripdata_reduced/' + row[0] + '.csv').encode('ascii', 'ignore'),
+                  index=False)
+
 
 
 def main():
