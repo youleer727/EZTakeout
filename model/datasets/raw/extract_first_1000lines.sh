f138a13ce03eb43e19113e06b8b46fcf66279f5d
vskyno6 <yq2212@columbia.edu>
2017-12-13 20:46:22 -0500

    added month_compare_to_overall

--- model/datasets/raw/extract_first_1000lines.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# this shell is used to extract first 1000 lines from a file into a new file
-# head -1000 input > output && sed -i '1,+999d' input
-head -1000 2016-06-first-1000.txt > 2016-06-first-1000.csv && sed -i '1,+999d' 2016-06-first-1000.txt
